ff3ef2cd84a388fff84f3f1e7201cb76370d217e
dharanbro <GiridharanR@VISUALBI.LOCAL>
2019-10-17 17:40:19 +0530

    port changes for heroku

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const Inert = require('@hapi/inert');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: (process.env.PORT || process.env.serverPort),
+        host: process.env.serverHost
     });
 
     await server.register(Inert);
